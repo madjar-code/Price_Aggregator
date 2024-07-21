6d3299f8ff65275fa5ea8b00428c6365ae2b0ea3
Madjar Evan <evanmadjar@gmail.com>
2024-07-21 15:37:03 +0300

    feat: Finally, create endpoints and register them

---
+++ app/routers/categories.py
@@ -0,0 +1,44 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from typing import List
+from .. import crud, models, schemas
+from ..database import SessionLocal, engine, get_db
+
+
+models.Base.metadata.create_all(bind=engine)
+
+
+router = APIRouter(
+    prefix='/categories',
+    tags=['categories'],
+    responses={404: {'description': 'Not found'}}
+)
+
+
+@router.post('/', response_model=schemas.Category)
+def create_category(
+    category: schemas.CategoryCreate,
+    db: Session = Depends(get_db)
+):
+    return crud.create_category(db=db, category=category)
+
+
+@router.get('/{category_id}', response_model=schemas.Category)
+def read_category(
+    category_id: int,
+    db: Session = Depends(get_db)
+):
+    db_category = crud.get_category(db, category_id=category_id)
+    if db_category is None:
+        raise HTTPException(status_code=404, detail='Category not found')
+    return db_category
+
+
+@router.get('/', response_model=List[schemas.Category])
+def read_categories(
+    skip: int = 0,
+    limit: int = 10,
+    db: Session = Depends(get_db)
+):
+    categories = crud.get_categories(db, skip, limit)
+    return categories
