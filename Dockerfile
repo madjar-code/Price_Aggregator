e454874c478d66cf11fa853c4ba1992e4e02d17c
Madjar Evan <evanmadjar@gmail.com>
2024-07-22 12:52:38 +0300

    chore: Connect PostgreSQL database using Docker

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
-FROM python:3.9-slim
+FROM python:3.7
 WORKDIR /app
-COPY requirements.txt .
-RUN pip install --no-cache-dir -r requirements.txt
 COPY ./app ./app
-COPY ./init_db.py ./init_db.py
-CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
+COPY requirements.txt requirements.txt
+RUN pip3 install -r requirements.txt
+
+# COPY ./init_db.py ./init_db.py
+# CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
--- app/database.py
+++ app/database.py
@@ -2,20 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./db.sqlite3'
+SQLALCHEMY_DATABASE_URL = "postgresql://username:password@db:5432/price_db"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    echo=True,
-    connect_args={"check_same_thread": False}
-)
-
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
-
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
 
 
