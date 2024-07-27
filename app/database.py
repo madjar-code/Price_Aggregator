00c9c937629a10011d54710996af22c1afefe875
Madjar Evan <evanmadjar@gmail.com>
2024-07-27 14:03:40 +0300

    chore: Add alembic migrations and .env file
    I have set up alembic migrations and plan
    to use them in the future. I also put the
    SQL URL in .env

--- app/database.py
+++ app/database.py
@@ -1,8 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "postgresql://username:password@db:5432/price_db"
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.getenv('SQLALCHEMY_DATABASE_URL')
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
